a35d166f96033ce6601f5a7c163591ae583bd7f9
Bekt <bekt17@gmail.com>
2013-10-17 10:34:26 -0500

    cpp solution

--- output/src/main.cpp
+++ output/src/main.cpp
@@ -75,7 +75,6 @@ int main() {
     for(int i=0; i <= n; i++) {
         memcpy(C, A, sizeof(int)*n);
         int hs = quicksort(C, left, right, i);
-        cout << i << ", " << hs << endl;
         if(hs < min) {
             min = hs;
             size = i;
